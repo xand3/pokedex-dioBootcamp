748aa5be651a70beda88193df870e6632c24ff49
Alexandre <xande.bastos321@gmail.com>
2023-08-27 23:09:42 -0300

    :construction: Buscando dados da pokeapi com fetch API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log("funciona")
+const offset = 0;
+const limit = 10;
+
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+  .then((res) => res.json())
+  .then((jsonBody) => console.log(jsonBody))
+  .catch((error) => console.log(error))
