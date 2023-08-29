7c53cb493d5847b9dd5bf2e54a2d1d872dbd62cc
Alexandre <xande.bastos321@gmail.com>
2023-08-28 21:48:41 -0300

    :construction: Integrando dados da pokeAPI na página

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,34 @@ const limit = 10;
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertPokemonToLi(pokemon) {
+  return `
+    <li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokemon.name}</span>
+      
+      <div class="detail">
+        <ol class="types">
+          <li class="type">grass</li>
+          <li class="type">poison</li>
+        </ol>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+      </div>
+    </li>
+  `;
+}
+
 fetch(url)
   .then((res) => res.json())
-  .then((jsonBody) => console.log(jsonBody))
-  .catch((error) => console.log(error))
+  .then((jsonBody) => jsonBody.results)
+  .then((pokemons) => {
+    
+
+    for(let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i]
+      const pokemonList = document.querySelector("#pokemonList")
+      pokemonList.innerHTML += convertPokemonToLi(pokemon)
+    }
+
+  })
+  .catch((error) => console.log(error));
