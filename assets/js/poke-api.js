79b5bc6df90750a886217e4ecbbcabb2b40ae314
Alexandre <xande.bastos321@gmail.com>
2023-08-29 20:13:37 -0300

    :construction: Implementado modelo do pokemon para a página.

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,24 @@
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon()
+  pokemon.number = pokeDetail.order
+  pokemon.name = pokeDetail.name
+
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+  const [type] = types
+
+  pokemon.types = types
+  pokemon.type = type
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+  return pokemon;
+}
+
 const PokeApi = {
   async getPokemonsDetails(pokemon) {
-    const res = await fetch(pokemon.url);
-    return await res.json();
+    const res = (await fetch(pokemon.url));
+    const pokemons = await res.json();
+
+    return convertPokeApiDetailToPokemon(pokemons);
   },
 
   async getPokemons(offset = 0, limit = 10) {
@@ -10,9 +27,10 @@ const PokeApi = {
     const res = await fetch(url);
     const jsonBody = await res.json();
     const pokemons = jsonBody.results;
+
     const detailsRequests = pokemons.map(PokeApi.getPokemonsDetails);
     const pokemonsDetails = await Promise.all(detailsRequests);
-    console.log(pokemonsDetails);
+
     return pokemonsDetails;
   }
 };
