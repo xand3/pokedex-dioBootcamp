63a32e79c39157db83b1d9c112d34e7ab06e0216
Alexandre <xande.bastos321@gmail.com>
2023-08-29 19:44:50 -0300

    :construction: Implementada função para buscar detalhes dos pokemons: getPokemonsDetails

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,18 @@
+function convertPokemonTypesToLi(pokemonTypes) {
+  return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
   return `
     <li class="pokemon">
-      <span class="number">#001</span>
+      <span class="number">#${pokemon.order}</span>
       <span class="name">${pokemon.name}</span>
       
       <div class="detail">
         <ol class="types">
-          <li class="type">grass</li>
-          <li class="type">poison</li>
+          ${convertPokemonTypesToLi(pokemon.types).join('')}
         </ol>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+        <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
       </div>
     </li>
   `;
@@ -17,8 +20,7 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.querySelector("#pokemonList");
 
-PokeApi.getPokemons().then((pokemons) => {
-  const listPokemons = pokemons.map((pokemon) => convertPokemonToLi(pokemon));
-
-  pokemonList.innerHTML += listPokemons.join("");
+PokeApi.getPokemons().then((pokemons = []) => {
+  const newHtml = pokemons.map(convertPokemonToLi).join("");
+  pokemonList.innerHTML = newHtml;
 });
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,12 +1,18 @@
 const PokeApi = {
+  async getPokemonsDetails(pokemon) {
+    const res = await fetch(pokemon.url);
+    return await res.json();
+  },
+
   async getPokemons(offset = 0, limit = 10) {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-    try {
-      const res = await fetch(url);
-      const jsonBody = await res.json();
-      return jsonBody.results;
-    } catch (error) {
-      return console.log(error);
-    }
+
+    const res = await fetch(url);
+    const jsonBody = await res.json();
+    const pokemons = jsonBody.results;
+    const detailsRequests = pokemons.map(PokeApi.getPokemonsDetails);
+    const pokemonsDetails = await Promise.all(detailsRequests);
+    console.log(pokemonsDetails);
+    return pokemonsDetails;
   }
 };
