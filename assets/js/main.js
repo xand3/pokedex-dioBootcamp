165efbebc7839370c89129f038cefc6126628a6c
Alexandre <xande.bastos321@gmail.com>
2023-08-30 22:14:20 -0300

    :construction: Atualizado design dos cards dos pokemons.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,10 @@ let offset = 0;
 function convertPokemonToLi(pokemon) {
   return `
     <li class="pokemon ${pokemon.type}">
-      <span class="number">#${pokemon.number}</span>
-      <span class="name">${pokemon.name}</span>
+      <div class="pokemon_identify">
+        <span class="name">${pokemon.name}</span>
+        <span class="number">#${pokemon.number}</span>
+      </div>
       
       <div class="detail">
         <ol class="types">
